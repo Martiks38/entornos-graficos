9790e854e4a49bac28743e2c6fae691b60473ad4
Giuliano Cappa <cappa.giuliano@gmail.com>
2026-08-22 15:09:37 -0300

    añadir solución ejercicio 2 de arrays, funciones

---
+++ PHP/Array_Func/Ejercicio2.php
@@ -0,0 +1,33 @@
+<?php
+  $matriz = array(
+    "x" => "bar",
+    12 => true
+  );
+
+  echo $matriz["x"];
+  echo $matriz[12];
+
+  // * Salida: bar1
+?>
+
+<?php
+  $matriz = array("unamatriz" => array(6 => 5, 13 => 9, "a" => 42));
+
+  echo $matriz["unamatriz"][6];
+  echo $matriz["unamatriz"][13];
+  echo $matriz["unamatriz"]["a"];
+
+  // * Salida: 5942
+?>
+
+<?php
+  $matriz = array(5 => 1, 12 => 2);
+  $matriz[] = 56;
+  $matriz["x"] = 42;
+
+  unset($matriz[5]);
+  unset($matriz);
+
+  // * No hay salida debido a que
+  // * no se utiliza ningún medio para ello.
+?>
